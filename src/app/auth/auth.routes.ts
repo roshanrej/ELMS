950b965a298b1339a368958e8cc40297089a0acf
Roshan Reji Varughese <roshin.r@polus.com>
2026-05-07 17:59:34 +0530

    Removed register to isolate leave domain from organizational system

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,9 @@
 import { Route } from "@angular/router"
 import { Login } from "./login/login"
-import { Register } from "./register/register"
+
 
 export const authRoutes : Route[] =[
     { path: 'login', redirectTo: '', pathMatch: 'full'},
     { path:'', component: Login },
-    {path:'register',component: Register}
+    
 ]
